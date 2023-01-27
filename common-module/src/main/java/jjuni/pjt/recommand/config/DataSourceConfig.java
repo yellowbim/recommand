3e0abf27d3d6981d7b5471dc1185ab8e7a5a1059
yellowbim <dlwjdwns010@gmail.com>
2023-01-27 17:53:31 +0900

    1. Spring 2.0부터는 HikariCP가 기본 지원이래서 yml만 수정 2. jpa도 많은 방식이 있지만 우선 기본적으로 사용하기 편한 spring data jpa를 사용
    TODO
    1. jpa를 사용해서 CRUD 작업 해보기

---
+++ common-module/src/main/java/jjuni/pjt/recommand/config/DataSourceConfig.java
@@ -0,0 +1,18 @@
+package jjuni.pjt.recommand.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+
+
+}
